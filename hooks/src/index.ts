dded7cfa1d2046fbef0329ca3f38b591cb39391e
NIL2000 <nilabhra09b.net@gmail.com>
2025-03-23 00:46:59 +0530

    Added temp webhook logic

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,15 +1,33 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
 
 //hooks.zapier.com/hooks/catch/:userId/:zapId
 
 const app = express();
-
-app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+const client = new PrismaClient();
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
   const { userId, zapId } = req.params;
+  const body = req.body;
+
+  await client.$transaction(async (tx) => {
+    const run = await client.zapRun.create({
+      data: {
+        zapId: zapId,
+        metaData: body,
+      },
+    });
+
+    await client.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id,
+      },
+    });
+  });
   //store in db a new trigger
   // push into a queue (redis/kafka)
+  res.status(201).json({
+    message: "Webhook received successfully",
+  });
 });
 
-app.listen(3000, () => {
-  console.log("The server has started!!!");
-});
+app.listen(3000);
